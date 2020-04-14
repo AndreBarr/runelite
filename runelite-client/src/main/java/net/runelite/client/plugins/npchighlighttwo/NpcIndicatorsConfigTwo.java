465cffc20d36a7762872de6ca39dc9e356ed00de
Andre Barrientos <abarrientos49@gmail.com>
2020-04-14 16:03:09 -0400

    Added npc indicator plugin that will show when attacks are about to happen (need to add attack animation ID manually)

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/npchighlighttwo/NpcIndicatorsConfigTwo.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright (c) 2018, Tomas Slusny <slusnucky@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.runelite.client.plugins.npchighlighttwo;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+import java.awt.*;
+
+@ConfigGroup("npcindicators")
+public interface NpcIndicatorsConfigTwo extends Config
+{
+	@ConfigItem(
+		position = 0,
+		keyName = "highlightStyle",
+		name = "Highlight Style",
+		description = "Highlight setting"
+	)
+	default RenderStyleTwo renderStyle()
+	{
+		return RenderStyleTwo.HULL;
+	}
+
+	@ConfigItem(
+		position = 1,
+		keyName = "npcToHighlight",
+		name = "NPCs to Highlight",
+		description = "List of NPC names to highlight"
+	)
+	default String getNpcToHighlight()
+	{
+		return "";
+	}
+
+	@ConfigItem(
+		position = 2,
+		keyName = "npcColor",
+		name = "Highlight Color",
+		description = "Color of the NPC highlight"
+	)
+	default Color getHighlightColor()
+	{
+		return Color.CYAN;
+	}
+
+	@ConfigItem(
+			position = 3,
+			keyName = "npcColorWarning",
+			name = "Highlight Color Warning",
+			description = "Color of the NPC highlight the tick before attacking"
+	)
+	default Color getHighlightColorWarning()
+	{
+		return Color.YELLOW;
+	}
+
+	@ConfigItem(
+			position = 4,
+			keyName = "npcColorAttack",
+			name = "Highlight Color Attack",
+			description = "Color of the NPC highlight when attacking"
+	)
+	default Color getHighlightColorAttack()
+	{
+		return Color.RED;
+	}
+
+	@ConfigItem(
+		position = 5,
+		keyName = "drawNames",
+		name = "Draw names above NPC",
+		description = "Configures whether or not NPC names should be drawn above the NPC"
+	)
+	default boolean drawNames()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		position = 6,
+		keyName = "drawMinimapNames",
+		name = "Draw names on minimap",
+		description = "Configures whether or not NPC names should be drawn on the minimap"
+	)
+	default boolean drawMinimapNames()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		position = 7,
+		keyName = "highlightMenuNames",
+		name = "Highlight menu names",
+		description = "Highlight NPC names in right click menu"
+	)
+	default boolean highlightMenuNames()
+	{
+		return false;
+	}
+
+	@ConfigItem(
+		position = 8,
+		keyName = "showRespawnTimer",
+		name = "Show respawn timer",
+		description = "Show respawn timer of tagged NPCs")
+	default boolean showRespawnTimer()
+	{
+		return false;
+	}
+}
